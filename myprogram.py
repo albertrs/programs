013de16911e36936900a71ee4babb6c9542e7948
albertrs <62557843+albertrs@users.noreply.github.com>
2020-03-23 16:57:55 +0100

    Add files via upload
    This is a new version of my program with excetptions.

--- myprogram.py
+++ myprogram.py
@@ -2,10 +2,14 @@
 import pickle
 
 def message(self):
-	print("""
-1[+] Add a new note
-2[+] Read one note existing
-			""")
+	print("""*****MyNotes*****
+
+	MENU
+
+	1[+] Add a new note
+	2[+] Read one note existing
+
+		""")
 	self.choose = int(input("Select one option > "))
 
 class myapp:
@@ -16,27 +20,34 @@ class myapp:
 
 	def add(self):
 		message(self)
-		if self.choose == 1:
-			self.tittle = input("\nTittle > ")
-			self.note = input("Note > ")
-			#ask for save
-			save = input("\nDo you want to save this note? Y/N: ")
-			if save == "y" or save == "Y":
-				#save files
-				file = open(str(self.tittle)+".pickl", "wb")
-				pickle.dump(self.tittle, file)
-				file.close()
-			elif save == "n" or save == "N":
-				return "\n¡¡¡ATENTION!!! > Your object called {0}.pickl was descarted.".format(self.tittle)
+		while self.choose != 1 and self.choose != 2:
+			message(self)
+			self.choose = int(input("Select one option > "))
+		else:
+			if self.choose == 1:
+				self.tittle = input("\nTittle > ")
+				self.note = input("Note > ")
+				#ask for save
+				save = input("\nDo you want to save this note? Y/N: ")
+				if save == "y" or save == "Y":
+					#save files
+					file = open(str(self.tittle)+".pickl", "wb")
+					pickle.dump(self.tittle, file)
+					file.close()
+				elif save == "n" or save == "N":
+					return "\n¡¡¡ATENTION!!! > Your object called {0}.pickl was descarted.".format(self.tittle)
 
 	def read(self):
-		if self.choose == 2:
-			name = input("File's name > ")
-			#read files
-			file2 = open(name+".pickl", "rb")
-			todo = pickle.load(file2)
-			return todo
-			file.close()
+		try:
+			if self.choose == 2:
+				name = input("File's name > ")
+				#read files
+				file2 = open(name+".pickl", "rb")
+				todo = pickle.load(file2)
+				return todo
+				file.close()
+		except FileNotFoundError:
+			return "[Errno 2] No such file or directory: '{0}.pickl'".format(name)
 
 obj = myapp("", "", "")
 print(obj.add())
